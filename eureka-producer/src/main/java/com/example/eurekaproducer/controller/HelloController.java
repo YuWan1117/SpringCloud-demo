57f535fee74531e6dd66a5a346c607debc86f127
liyingxiao <liyingxiao.kkworld.com>
2021-12-09 10:53:47 +0800

    使用OpenFeign替换RestTemplate

--- eureka-producer/src/main/java/com/example/eurekaproducer/controller/HelloController.java
+++ eureka-producer/src/main/java/com/example/eurekaproducer/controller/HelloController.java
@@ -1,16 +1,23 @@
 package com.example.eurekaproducer.controller;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @Slf4j
 public class HelloController {
 
-    @GetMapping("sayHello")
-    public String sayHello(String user) {
+    @RequestMapping("sayHello")
+    public String sayHello(@RequestParam String user) {
         log.info("receive a sayHello request, user={}", user);
         return "Hello " + user;
+    }
+
+    @RequestMapping("hello")
+    public String hello() {
+        log.info("receive a hello request");
+        return "Hello World.";
     }
 }
